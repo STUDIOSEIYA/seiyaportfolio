9bf367c7abe3ecb88e8a44f38c1d168b518e2227
ACG1135FX <75176377+hellocap@users.noreply.github.com>
2024-06-01 23:43:08 +0100

    switched video source from youtube to raw video

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Footer from "../components/Footer"
 import { HomePageWrapper } from "../styles/HomeStyles";
 
 export default function Home() {
-  const {titlePage, setTitlePage} = useStateContext();
+  const { titlePage, setTitlePage } = useStateContext();
 
   useEffect(() => {
-    setTitlePage("Seiya Portfolio")
+    setTitlePage("Seiya Portfolio");
   }, []);
 
   return (
@@ -16,36 +16,33 @@ export default function Home() {
       <Head>
         <title>{titlePage}</title>
         <meta name="description" content="Seiya Portfolio Website" />
-        <meta property="twitter:title" content="Seiya Portfolio Website"/>
-        <meta property="twitter:description" content="Seiya Portfolio Website"/>
-        <meta property="og:description" content="Seiya Portfolio Website"/>
-        <meta property="og:title" content="Midnight Breeze"/>
-        <meta property="og:url" content="https://www.seiyastudio.com/"/>
-        <meta property="og:type" content="website"/>
+        <meta property="twitter:title" content="Seiya Portfolio Website" />
+        <meta property="twitter:description" content="Seiya Portfolio Website" />
+        <meta property="og:description" content="Seiya Portfolio Website" />
+        <meta property="og:title" content="Midnight Breeze" />
+        <meta property="og:url" content="https://www.seiyastudio.com/" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.png" />
-        <meta property="og:image" content="https://www.seiyastudio.com/thumbnail.png"/>
-        <meta property="twitter:image" content="https://www.seiyastudio.com/thumbnail.png"/>
+        <meta property="og:image" content="https://www.seiyastudio.com/thumbnail.png" />
+        <meta property="twitter:image" content="https://www.seiyastudio.com/thumbnail.png" />
       </Head>
-     
 
       <HomePageWrapper>
- 
-      <section className="showcase">
-        <div  className="videoWrapper">
-        <iframe
-       
-          src="https://www.youtube.com/embed/WeA7yYSCiRE?autoplay=1&mute=1&loop=1&playlist=WeA7yYSCiRE&controls=0"
-          title="Seiya Showreel"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      </section>
+        <section className="showcase">
+          <div className="videoWrapper">
+            <video
+              src="/assets/video/showreel.mp4"
+              autoPlay
+              muted
+              loop
+              controls={false}
+              width="100%"
+              height="100%"
+            />
+          </div>
+        </section>
         <Footer />
       </HomePageWrapper>
-
-      
     </div>
-  )
+  );
 }

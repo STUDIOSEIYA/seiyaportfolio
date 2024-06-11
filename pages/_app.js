67ae77bb65c6ed3014dd215fcf38141268775924
ACG1135FX <75176377+hellocap@users.noreply.github.com>
2024-06-11 09:53:11 +0100

    updated video source

--- pages/_app.js
+++ pages/_app.js
@@ -3,39 +3,32 @@ import Layout from '../components/Layout.js'
 import { StateContext } from "../lib/context"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function MyApp({ Component, pageProps }) {
-
-
-
   return (
- 
+    <StateContext>
+      <Layout>
+        <video
+          src="/assets/video/showreel.mp4"
+          autoPlay
+          muted
+          loop
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: 0
+          }}
+        ></video>
 
-      <StateContext>
-        <Layout>
-        <iframe 
-  src="https://www.youtube.com/embed/WeA7yYSCiRE?autoplay=1&mute=1&controls=0&showinfo=0&rel=0" 
-  allow="autoplay; fullscreen"
-  allowFullScreen
-  style={{
-    width: "100%", 
-    height: "100%", 
-    objectFit: "cover", 
-    position: "fixed", 
-    top: 0, 
-    left: 0, 
-    bottom: 0, 
-    right: 0, 
-    zIndex: 0
-  }}
-></iframe>
-
-          <Component {...pageProps} />
-        </Layout>
-      </StateContext>
-
-
-    )
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer"
 import { HomePageWrapper } from "../styles/HomeStyles";
 
 export default function Home() {
-  const {titlePage, setTitlePage} = useStateContext();
+  const { titlePage, setTitlePage } = useStateContext();
 
   useEffect(() => {
     setTitlePage("Seiya Portfolio")
@@ -16,36 +16,25 @@ export default function Home() {
       <Head>
         <title>{titlePage}</title>
         <meta name="description" content="Seiya Portfolio Website" />
-        <meta property="twitter:title" content="Seiya Portfolio Website"/>
-        <meta property="twitter:description" content="Seiya Portfolio Website"/>
-        <meta property="og:description" content="Seiya Portfolio Website"/>
-        <meta property="og:title" content="Midnight Breeze"/>
-        <meta property="og:url" content="https://www.seiyastudio.com/"/>
-        <meta property="og:type" content="website"/>
+        <meta property="twitter:title" content="Seiya Portfolio Website" />
+        <meta property="twitter:description" content="Seiya Portfolio Website" />
+        <meta property="og:description" content="Seiya Portfolio Website" />
+        <meta property="og:title" content="Seiya Portfolio Website" />
+        <meta property="og:url" content="https://www.seiyastudio.com/" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.png" />
-        <meta property="og:image" content="https://www.seiyastudio.com/thumbnail.png"/>
-        <meta property="twitter:image" content="https://www.seiyastudio.com/thumbnail.png"/>
+        <meta property="og:image" content="https://www.seiyastudio.com/thumbnail.png" />
+        <meta property="twitter:image" content="https://www.seiyastudio.com/thumbnail.png" />
       </Head>
-     
 
       <HomePageWrapper>
- 
-      <section className="showcase">
-        <div  className="videoWrapper">
-        <iframe
+        <section className="showcase">
+          <div className="videoWrapper">
        
-          src="https://www.youtube.com/embed/WeA7yYSCiRE?autoplay=1&mute=1&loop=1&playlist=WeA7yYSCiRE&controls=0"
-          title="Seiya Showreel"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      </section>
+          </div>
+        </section>
         <Footer />
       </HomePageWrapper>
-
-      
     </div>
   )
 }

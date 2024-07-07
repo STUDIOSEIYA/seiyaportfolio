2e42d9ece02cac9b3409464ac004b34e24e21011
Caprica <caprica-dev@pm.me>
2024-07-07 16:27:12 +0200

    mobile version testing

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <div className="mobile-message">
-        <div className="logo-container">
-          <Image
-            src="/assets/img/logo.png" // Adjust the path as necessary
-            alt="Logo"
-            width={100} // Adjust the dimensions as necessary
-            height={100}
-          />
-        </div>
-        <p>Please switch to a desktop for the best experience.</p>
-      </div>
-      <div className="main-content">
-        {children}
-      </div>
-      <Footer />
+      <Navbar /> 
+        <div> {children} </div>
+   
+
     </>
   );
 };
--- pages/about.js
+++ pages/about.js
@@ -16,52 +16,43 @@ export default function About() {
     <AboutPageWrapper>
       <div className="about-home">
         <div className="left-img">
-        <ThreeDModel />
-        
+          <ThreeDModel />
         </div>
 
-        <div className="text-right">
-        <h1>About me</h1>
-        <p>I'm Thomas Travert, Graphic / Motion Designer 2D-3D & Illustrator based in Tokyo </p>
-        <h1>Awards</h1>
-        <div className="awards-twocol">
-
-          <div className="awards-left">
-            <h2>ADOBE x SONY ANIMATIONS</h2>
-            <p>Contest Spider Man into the spiderverse</p>
+        <div className="desktop-only">
+          <div className="text-right">
+            <h1>About me</h1>
+            <p>I'm Thomas Travert, Graphic / Motion Designer 2D-3D & Illustrator based in Tokyo</p>
+            <h1>Awards</h1>
+            <div className="awards-twocol">
+              <div className="awards-left">
+                <h2>ADOBE x SONY ANIMATIONS</h2>
+                <p>Contest Spider Man into the spiderverse</p>
+              </div>
+              <div className="awards-right">
+                <h2>TOP 25 WORLDLINE MARSHMELLO</h2>
+                <p>Content Adobe x MARSHMELLO</p>
+              </div>
+            </div>
+            <h1>References</h1>
+            <div className="awards-twocol">
+              <div className="awards-left">
+                <p><span>Adobe Design</span> / Daft Punk 2070 Remastered</p>
+                <p><span>Adobe Creative Cloud Live</span> / Portfolio Review</p>
+                <p><span>Adobe France</span> / Daft Punk 2070</p>
+                <p><span>Adobe France</span> / Coca Cola #CokexAdobexYou</p>
+              </div>
+              <div className="awards-right">
+                <p><span>Wacom</span> / Artwork of #CIntiq22HD</p>
+                <p><span>Adobe France</span> / Black and White Illustrations</p>
+                <p><span>XP Pen France</span> / Ambassadeur France</p>
+                <p><span>Insydium XParticles</span> / Motion Design</p>
+              </div>
+            </div>
           </div>
-          <div className="awards-right">
-            <h2>TOP 25 WORLDLINE MARSHMELLO</h2>
-            <p>Content Adobe x MARSHMELLO</p>
-          </div>
-
-
         </div>
-        <h1>References</h1>
-        <div className="awards-twocol">
-
-          <div className="awards-left">
-        
-            <p> <span>Adobe Design</span> / Daft Punk 2070 Remastered</p>
-            <p> <span>Adobe Creative Cloud Live</span> / Portfolio Review</p>
-            <p> <span>Adobe France</span> / Daft Punk 2070</p>
-            <p> <span>Adobe France</span> / Coca Cola  #CokexAdobexYou</p>
-          </div>
-          <div className="awards-right">
-          
-          <p> <span>Wacom</span> / Artwork of #CIntiq22HD</p>
-            <p> <span>Adobe France</span> / Black and White Illustrations</p>
-            <p> <span>XP Pen France</span> / Ambassadeur France</p>
-            <p> <span>Insydium XParticles</span> / Motion Design</p>
-          </div>
-
-
-        </div>
-        </div>
-
-      
       </div>
-      <div className="logos-clients">
+      <div className="desktop-only logos-clients">
         <div className="background-image"></div>
       </div>
     </AboutPageWrapper>

da8ae39daddcc46804ee15b1425fc01aaffd8827
Caprica <caprica-dev@pm.me>
2024-07-07 16:11:56 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,26 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar /> 
-        <div> {children} </div>
-   
-
+      <Navbar />
+      <div className="mobile-message">
+        <div className="logo-container">
+          <Image
+            src="/assets/img/logo.png" // Adjust the path as necessary
+            alt="Logo"
+            width={100} // Adjust the dimensions as necessary
+            height={100}
+          />
+        </div>
+        <p>Please switch to a desktop for the best experience.</p>
+      </div>
+      <div className="main-content">
+        {children}
+      </div>
+      <Footer />
     </>
   );
 };
